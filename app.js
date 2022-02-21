27fdc71bb9db4faaaa56b7b4fb59a7ddb458ca27
Farhaan <fari3001_14@hotmail.com>
2022-02-22 02:51:42 +1000

    looping through clicks

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'www.facebook.com',
             showBooks: false,
             title: 'the final empire',
             author: 'An authore',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                { title: 'name of the wind', author: 'whatever', isFav: true },
+                { title: 'name of the bind', author: 'whatever2', isFav: false },
+                { title: 'name of the tind', author: 'whatever3', isFav: true }
+            ]
         }
     },
     methods: {
@@ -14,6 +22,15 @@ const app = Vue.createApp({
 
         handleEvent(e) {
             console.log(e, e.type)
+        },
+
+        handleMouseMove (e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        handleClass(item) {
+            console.log(item)
+            item.isFav = !item.isFav
         }
     }
 })
